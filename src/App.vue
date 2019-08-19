3c71e6417fd08325ed93f5b1f10ef430cf547527
elkaput <54052999+elkaput@users.noreply.github.com>
2019-08-20 09:39:55 +1000

    Added header navigation in App.vue (shared by other pages).

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
-  <div id="app">
+  <div id="app-vue"> <!-- elkaput 20-Aug-2019: previous id="app" which clashes with one in index.html -->
+    <header class="site-header">
+      <b-navbar type="dark" fixed="top">
+        <b-navbar-brand href="/">World War of Ants</b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item to="DevLogs">Devloper Logs</b-nav-item>
+            <b-nav-item to="About">About Us</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </header>
     <router-view />
   </div>
 </template>
